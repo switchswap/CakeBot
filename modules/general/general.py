886ac6116f4af40600641fa43591e8e670e1436c
Swapnil Patel <sp70215@gmail.com>
2020-06-26 22:03:14 -0700

    Create `whois` command
    Command prints member info

--- modules/general/general.py
+++ modules/general/general.py
@@ -1,4 +1,4 @@
-from discord import Embed
+from discord import Embed, Member
 from discord.ext import commands
 from random import randint
 from datetime import datetime
@@ -63,8 +63,42 @@ class General(commands.Cog):
         await ctx.send(embed=embed)
 
     @commands.command(name="whois")
-    async def whois(self, ctx, mention):
-        pass
+    @commands.guild_only()
+    async def whois(self, ctx, member: Member):
+        """
+        Show user information
+        """
+        # Get user roles
+        roles = "\n".join(role.name for role in member.roles)
+        guild_join_date = member.joined_at.strftime('%m-%d-%Y %H:%M:%S UTC')
+        discord_join_date = member.created_at.strftime('%m-%d-%Y %H:%M:%S UTC')
+        nick = member.nick if member.nick is not None else "N/A"
+        # Create response
+        embed = Embed(color=self.bot.default_color)
+        embed.set_author(name=f"{member}'s info")
+        embed.set_thumbnail(url=f"{member.avatar_url}")
+        embed.add_field(name="Name", value=f"{member.name}", inline=True)
+        embed.add_field(name="Nick", value=f"{nick}", inline=True)
+        embed.add_field(name="Roles", value=roles)
+        # Guild join date can be None so default value is needed
+        embed.add_field(name="Guild Join Date", value=guild_join_date if guild_join_date is not None else "N/A")
+        embed.add_field(name="Discord Join Date", value=discord_join_date)
+        await ctx.send(embed=embed)
+
+    @commands.command(name="avatar")
+    @commands.guild_only()
+    async def avatar(self, ctx, member: Member = None):
+        """
+        Show user avatar
+        """
+        # Default to message author
+        if member is None:
+            member = ctx.author
+        embed = Embed(color=self.bot.default_color)
+        embed.title = f"{member.display_name}'s avatar"
+        embed.url = str(member.avatar_url)
+        embed.set_image(url=member.avatar_url)
+        await ctx.send(embed=embed)
 
     @staticmethod
     def _valid_boundary(number):
@@ -81,6 +115,10 @@ class General(commands.Cog):
             return False
 
         return True
+
+    @staticmethod
+    def _datetime_to_string(time: datetime) -> str:
+        return time.strftime("%m/%d/%Y")
 
 
 def setup(bot):
