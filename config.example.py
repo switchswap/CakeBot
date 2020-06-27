ae28e77fd22edfc5fc291c5713cc30b16ad3d0b0
Swapnil Patel <sp70215@gmail.com>
2020-06-27 13:05:49 -0700

    Update config example

--- config.example.py
+++ config.example.py
@@ -1,3 +1,9 @@
-# sample_token = ""
+# General config
 bot_token = ""
+bot_prefixes = ['!']  # A list of bot prefix values eg: ['!', "c!", "cake"]
+default_color = 0xf77d81  # An integer value representing color
+approved_bots = []  # List of integer bot ID's
+
+# Module specific config
+# osu
 osu_api_key = ""
